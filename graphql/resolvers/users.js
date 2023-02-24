cb5449f34faab291106cc5b0402b1ded56c7aaec
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-24 18:59:30 +0530

    register resolver created

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,57 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
+
+module.exports = {
+  Mutation: {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      //validate user data-ex- checking for empty fields, passwords not match
+      //make sure user does not already exist--cheking for unique username
+
+
+
+      //hash password before storing into DB & create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      //save to DB
+      const res = await newUser.save();
+    //   console.log("this is res: ", res, res.id, res._doc);
+
+     
+      //create auth token
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
+
+//here impelement the resolver for register mutation
+
+//parent--gives u the result of what was the input from the last step
