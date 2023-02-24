751f2a14271248015393a55a3437898d52e1ca92
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-24 14:01:16 +0530

    refactored the index.js

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-//here  now we need to have something called type definitions
-//step1
-//importing one of the dependencies of apollo server
-const gql = require("graphql-tag");
-
 const mongoose = require("mongoose");
 
-
-const Post = require("./models/Post");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
-
-//step2
-//here write your grpahql types
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-//so we've query-- getPosts. now create a resolver for this
-
-//resolvers--
-const resolvers = {
-  Query: {
-    async getPosts() {
-      //it better to wrap in try catch, coz- if your query fails, it might stops your actual server, so that's why handle the catch separtly
-
-
-      //so we fetch everything from DB, & filter accoding to client request in here & then send to client.
-      //and the range of client request is also defined by us in graphQL
-
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
-  },
-};
 
 //setup the apollo sever
 const server = new ApolloServer({
