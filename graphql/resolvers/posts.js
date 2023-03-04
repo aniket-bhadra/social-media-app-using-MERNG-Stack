d1b3aacd847461f73eb0119908e5bbcdbbcf3514
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-04 22:33:08 +0530

    liking post & unliking post functionality implemented

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -59,6 +59,29 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      // console.log(post);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //post already likes it, now unlike it here
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //post not liked, now like it here
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
