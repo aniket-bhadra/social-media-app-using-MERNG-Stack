8874ac9964622ce7abc01fad2c3756bb5e4a00b7
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-27 21:04:02 +0530

    Register form created

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-export default Register
+  const onChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  
+  };
+
+  return (
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate>
+        <h1>Register</h1>
+        <Form.Input
+          label="Username"
+          placeholder="Username..."
+          name="username"
+          type="text"
+          value={values.username}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Email"
+          placeholder="Email..."
+          name="email"
+          type="email"
+          value={values.email}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Password"
+          placeholder="Password..."
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Confirm Password"
+          placeholder="Confirm Password..."
+          name="confirmPassword"
+          type="password"
+          value={values.confirmPassword}
+          onChange={onChange}
+        />
+        <Button type="submit" primary>
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+
+
+export default Register;
