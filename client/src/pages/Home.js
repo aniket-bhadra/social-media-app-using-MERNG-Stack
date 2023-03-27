7e8d9cd12f3f9471d7eab7851869a4959af00000
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-27 15:48:39 +0530

    PostCard component created & added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,18 +2,29 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
-  if (data) {
-    console.log(data.getPosts);
-  }
-
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          data &&
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
 
