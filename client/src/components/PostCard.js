ba5bd548ba0a87f3f66dbf79e416f6d3117caa42
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-27 17:56:26 +0530

    like-button added

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Card, Icon, Label, Image } from "semantic-ui-react";
+import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import moment from "moment";
 
 const PostCard = ({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) => {
+  const likePost = () => {
+    console.log("post is liked!!!");
+  };
   return (
     <Card fluid>
       <Card.Content>
@@ -14,11 +18,20 @@ const PostCard = ({
           src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <p>buttons here </p>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="blue" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
       </Card.Content>
     </Card>
   );
