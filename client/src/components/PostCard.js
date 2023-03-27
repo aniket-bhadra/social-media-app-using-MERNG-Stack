5f513bcbcd8ea169c82300b8f0c67c54c14de800
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-27 18:08:54 +0530

    comment-button added

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -9,6 +9,9 @@ const PostCard = ({
   const likePost = () => {
     console.log("post is liked!!!");
   };
+  const commentOnPost = () => {
+    console.log("Comment on post!!");
+  };
   return (
     <Card fluid>
       <Card.Content>
@@ -30,6 +33,14 @@ const PostCard = ({
           </Button>
           <Label basic color="blue" pointing="left">
             {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button color="purple" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="purple" pointing="left">
+            {commentCount}
           </Label>
         </Button>
       </Card.Content>
