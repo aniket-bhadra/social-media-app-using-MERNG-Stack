4298c71bcb8fafd0599c9db957bc44c4d8593412
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-27 14:03:41 +0530

    fetching post testing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
-export default Home
+  if (data) {
+    console.log(data.getPosts);
+  }
+
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  );
+};
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+
+export default Home;
