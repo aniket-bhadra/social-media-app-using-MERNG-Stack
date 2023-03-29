8c2e6df15209bb7ac5df007681242e411734b9b2
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-29 22:29:50 +0530

    Register.js refactored with custom-hook

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,40 +4,21 @@ import { Button, Form } from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
+import { useForm } from "../util/hooks";
+
 const Register = () => {
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
-  const [values, setValues] = useState({
+
+  const { onChange, onSubmit, values } = useForm(registerUser, {
     username: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const onChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  //after passing REGISTER_USER, then the pass the option object where update() will be triggered if the mutation is succefully executed. proxy- hold meta deta, result-hold the result of the mutation
-  //then also we need to give varibles coz--this is mutation
-  //need to send like this--
-  // varibles: {
-  //   username: values.username//just like this add rest of theree varibles value
-  // }
-  //but since values contain also the same like structure so simply put it values
-
-  //then after all of that trigger the addUser() function when you want to submit the form.
-
-  //if we get errors onError() triggered
-
-  //untill the mutation is executing the loading varible is--true. after the execution of mutation then the lodaing varible become false regardless whether it is succeed or failed. then if succeed--> update gets called, if failed then OnError gets called.
-
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
     update(proxy, result) {
-      console.log(result);
       navigate("/", {
         replace: true,
       });
@@ -48,10 +29,10 @@ const Register = () => {
     variables: values,
   });
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  //revisit this imp function trick
+  function registerUser() {
     addUser();
-  };
+  }
 
   return (
     <div className="form-container">
