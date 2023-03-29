d1019bfa41a5a3d03b0b245f4fc6ac6c633d2976
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-29 23:29:27 +0530

    reducer created

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -6,4 +6,22 @@ const AuthContext = createContext({
   logout: () => {},
 });
 
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+}
 
