e31a61fd6458e2538bbcde9472e4b7d48a488ca9
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-30 11:36:40 +0530

    wrapped the context provider to all the componentes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import { AuthProvider } from "./context/auth";
+
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -11,14 +13,16 @@ import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <Container>
-      <MenuBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Container>
+    <AuthProvider>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </AuthProvider>
   );
 }
 
