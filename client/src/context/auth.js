b7b15bd99e2bcf1a84f1b2432055e35c3d5ac899
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-30 11:35:40 +0530

    context created

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,8 +1,8 @@
-import React, { createContext } from "react";
+import React, { useReducer, createContext } from "react";
 
 const AuthContext = createContext({
   user: null,
-  login: (data) => {},
+  login: (userData) => {},
   logout: () => {},
 });
 
@@ -25,3 +25,30 @@ function authReducer(state, action) {
   }
 }
 
+function AuthProvider(props) {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  function login(userData) {
+    dispatch({
+      type: "LOGIN",
+      payload: userData,
+    });
+  }
+
+  function logout() {
+    dispatch({
+      type: "LOGOUT",
+    });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user: state.user, login, logout }}
+      {...props}
+    />
+  );
+}
+
+export { AuthContext, AuthProvider };
