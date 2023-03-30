34f95a4353b3859e2a7c6c73156f564773856135
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-31 04:18:20 +0530

    modified the request to attach jwt for accessing protected api calls

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,6 +6,7 @@ import { InMemoryCache } from "@apollo/client";
 import { createHttpLink } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:5000/",
@@ -13,9 +14,24 @@ const httpLink = createHttpLink({
 
 //there pass some opitions, where uri is pointing to our GraphQL server (endpoint for our server, in production it will be different)
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+
+  //this return will modify the current request.
+
+  // this kind of merge the existing headers of the request with this headers object we gave. so it will just add this authorization header
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+//afterthat concatenate it to httpLink but it needs to go before httpLink, this will actually add the token to our request and allow us to successfully send any protected api calls
+
 //create client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
