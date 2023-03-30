d8ff63be54a059ba88f89e93106678e3e4db4851
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-03-30 14:42:12 +0530

    made the MenuBar dynamic based on the user is logged in or not

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../context/auth";
+
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
   const path = pathname === "/" ? "home" : pathname.slice(1);
   const [activeItem, setActiveItem] = useState(path);
@@ -10,7 +13,19 @@ function MenuBar() {
   // console.log("a");
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const dynamicMenu = user ? (
+    <Menu pointing secondary size="massive" color="blue">
+      <Menu.Item name="home" active as={Link} to="/" />
+
+      <Menu.Menu position="right">
+        <Menu.Item
+          name={user.username}
+          style={{ color: "#B413EC", fontWeight: "bold", fontStyle: "italic" }}
+        />
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="blue">
       <Menu.Item
         name="home"
@@ -38,6 +53,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return dynamicMenu;
 }
 
 export default MenuBar;
