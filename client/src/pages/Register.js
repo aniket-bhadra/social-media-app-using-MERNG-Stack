6fb642d774aef377531ceba47f546eaf6d8346a7
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-03-17 20:28:38 +0530

    Login & Register page styles updated

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,14 @@
 import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Form } from "semantic-ui-react";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Container,
+  Header,
+  Segment,
+  Message,
+  Divider,
+} from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
@@ -32,65 +40,122 @@ const Register = () => {
     variables: values,
   });
 
-  //revisit this imp function trick
   function registerUser() {
     addUser();
   }
 
   return (
-    <div className="form-container">
-      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
-        <h1>Register</h1>
-        <Form.Input
-          label="Username"
-          placeholder="Username..."
-          name="username"
-          type="text"
-          value={values.username}
-          error={errors.username ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Email"
-          placeholder="Email..."
-          name="email"
-          type="email"
-          value={values.email}
-          error={errors.email ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Password"
-          placeholder="Password..."
-          name="password"
-          type="password"
-          value={values.password}
-          error={errors.password ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Confirm Password"
-          placeholder="Confirm Password..."
-          name="confirmPassword"
-          type="password"
-          value={values.confirmPassword}
-          error={errors.confirmPassword ? true : false}
-          onChange={onChange}
-        />
-        <Button type="submit" primary>
-          Register
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className="ui error message">
-          <ul className="list">
-            {Object.values(errors).map((value) => (
-              <li key={value}>{value}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <Container text>
+      <div
+        className="form-container"
+        style={{ paddingTop: "2rem", paddingBottom: "2rem" }}
+      >
+        <Segment
+          raised
+          padded
+          style={{
+            background: "#F5F8FC",
+            borderRadius: "10px",
+            boxShadow: "0 0 15px rgba(0,0,0,0.1)",
+          }}
+        >
+          <Header as="h1" textAlign="center" style={{ color: "#0D2B4B" }}>
+            Create New Account
+            <Divider
+              style={{
+                borderColor: "#FF7A45",
+                width: "60px",
+                margin: "0.5rem auto",
+              }}
+            />
+          </Header>
+          <Form
+            size="large"
+            onSubmit={onSubmit}
+            noValidate
+            className={loading ? "loading" : ""}
+          >
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              label="Username"
+              placeholder="Username..."
+              name="username"
+              type="text"
+              value={values.username}
+              error={errors.username ? true : false}
+              onChange={onChange}
+            />
+            <Form.Input
+              fluid
+              icon="mail"
+              iconPosition="left"
+              label="Email"
+              placeholder="Email..."
+              name="email"
+              type="email"
+              value={values.email}
+              error={errors.email ? true : false}
+              onChange={onChange}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              label="Password"
+              placeholder="Password..."
+              name="password"
+              type="password"
+              value={values.password}
+              error={errors.password ? true : false}
+              onChange={onChange}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              label="Confirm Password"
+              placeholder="Confirm Password..."
+              name="confirmPassword"
+              type="password"
+              value={values.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              onChange={onChange}
+            />
+            <Button
+              type="submit"
+              fluid
+              size="large"
+              style={{
+                backgroundColor: "#0D2B4B",
+                color: "white",
+              }}
+              disabled={loading}
+            >
+              Register
+            </Button>
+          </Form>
+
+          {Object.keys(errors).length > 0 && (
+            <Message error>
+              <ul className="list" style={{ padding: 0 }}>
+                {Object.values(errors).map((value) => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+            </Message>
+          )}
+
+          <Message>
+            Already have an account?{" "}
+            <Link to="/login" style={{ color: "#FF7A45" }}>
+              Sign In
+            </Link>
+          </Message>
+        </Segment>
+      </div>
+    </Container>
   );
 };
 
